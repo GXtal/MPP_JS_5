8703d73608aafa2ab78ff775e3431fd6f8ec96d5
GXtal <zazaza00133@gmail.com>
2024-03-29 12:35:53 +0300

    cum

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import OperatorInfo from './pages/OperatorInfo';
 import OperatorList from './pages/OperatorList';
@@ -7,35 +7,35 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from "./partials/Header";
 import PrivateContextLayout from "./components/PrivateContextLayout";
-import {useContext} from "react";
-import {AuthContext} from "./contexts/AuthContext";
+import { useContext } from "react";
+import { AuthContext } from "./contexts/AuthContext";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles.css'
 
-function App(){  
+function App() {
 
-  const {loginFail} = useContext(AuthContext)
+  const { loginFail } = useContext(AuthContext)
 
 
-    return <div className="App">
+  return <div className="App">
 
-          <Header/>
-          <Routes>
-            <Route path="/" element={<Home/>}/> 
-            <Route element={<PrivateContextLayout loginFail={loginFail}/>}>
-            <Route path="/operators" element={<OperatorList/>}/>
-            <Route path="/operator/:id" element={<OperatorInfo/>}/>
-            
-            </Route>
-            
-            <Route path="/login" element={<Login/>}/> 
-            
-            <Route path="/register" element={<Register/>}/> 
-          </Routes>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route element={<PrivateContextLayout loginFail={loginFail} />}>
+        <Route path="/operators" element={<OperatorList />} />
+        <Route path="/operator/:id" element={<OperatorInfo />} />
 
-    </div>
-  }
+      </Route>
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
+    </Routes>
+
+  </div>
+}
 
 /* 
             
